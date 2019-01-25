873e3f11b91c6b5b4bb48aea59af3dc82870dc00
Pavel <lazukin.g08@gmail.com>
2019-01-25 17:51:54 +0300

    first view added

--- test_app/models.py
+++ test_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Profile(models.Model):
-    vk_id = models.CharField(max_length=200)
+    vk_id = models.TextField()
     first_name = models.TextField()
     last_name = models.TextField()
     access_token = models.TextField()
--- test_app/views.py
+++ test_app/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def greeting(request):
+    return render(request, 'test_app/greeting.html', {})
