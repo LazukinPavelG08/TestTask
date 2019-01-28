9e2e8236bdd676bd74ec4b88b9640b37bcebcad4
Pavel <lazukin.g08@gmail.com>
2019-01-28 09:00:25 +0300

    second view added

--- test_app/urls.py
+++ test_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.greeting, name='greeting'),
+    url(r'^vk_api', views.vk_api, name='vk_api'),
 ]
--- test_app/views.py
+++ test_app/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def greeting(request):
     return render(request, 'test_app/greeting.html', {})
+
+
+def vk_api(request):
+    return render(request, 'test_app/vk_api.html', {})
